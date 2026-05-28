3daa0309a192b103e548948b20aeb32dcf379322
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-05-28 08:58:26 +0800

    experiment: window size

--- cmd/03_videoReadWrite/main.go
+++ cmd/03_videoReadWrite/main.go
@@ -7,22 +7,24 @@ import (
 	"gocv.io/x/gocv"
 )
 
-const vid = "images/small.mp4"
+// const src = "images/small.mp4"
+const src = 0
 
 func main() {
-	cap, err := gocv.OpenVideoCapture(vid)
+	//cap, err := gocv.OpenVideoCapture(src)
+	cap, err := gocv.OpenVideoCapture(src) // 0: /dev/video0
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer cap.Close()
 
 	fps := cap.Get(gocv.VideoCaptureFPS)
-	fmt.Printf("FPS of %s is %v frames per second.\n", vid, fps)
+	fmt.Printf("FPS of %v is %v frames per second.\n", src, fps)
 
 	frameCount := cap.Get(gocv.VideoCaptureFrameCount)
-	fmt.Printf("%s has %g, %s encoded frames.\n", vid, frameCount, cap.CodecString())
+	fmt.Printf("%v has %g, %s encoded frames.\n", src, frameCount, cap.CodecString())
 
-	w := gocv.NewWindow(vid)
+	w := gocv.NewWindow("Video Capture")
 	defer w.Close()
 
 	img := gocv.NewMat()
@@ -31,15 +33,17 @@ func main() {
 	fmt.Println("Press q to quit.")
 	for {
 		if ok := cap.Read(&img); !ok {
-			fmt.Println("error reading: ", vid)
+			fmt.Println("error reading: ", src)
 			return
 		}
 
-		w.IMShow(img)
-		if key := w.WaitKey(int(1.0 / fps * 1000)); key == 'q' {
+		//if key := w.WaitKey(int(1.0 / fps * 1000)); key == 'q' {
+		if key := w.WaitKey(1); key == 'q' {
 			fmt.Println("'q' pressed. Exiting now.")
 			break
 		}
+		w.SetWindowProperty(gocv.WindowPropertyFullscreen, gocv.WindowFullscreen)
+		w.IMShow(img)
 	}
 
 }
