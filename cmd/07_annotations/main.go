70c7c9fec8d593485525743fc63d95a18fd2c125
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-06-13 13:59:18 +0800

    Squashed commit of the following:
    commit 65909788ae9dc1797b4cce23b8dfe7608f8ca972
    Author: Loh Siu Yin <siuyin@beyondbroadcast.com>
    Date:   Sat Jun 13 13:56:47 2026 +0800
    
        feat: add antialiasing
    
    commit f652863c0f4fc8c312c0a0d808689d61f6d729db
    Author: Loh Siu Yin <siuyin@beyondbroadcast.com>
    Date:   Sat Jun 13 13:43:32 2026 +0800
    
        feat: add alpha blending
    
    commit f3001717ea748d4065af9b1ab6cc3d223aac1777
    Author: Loh Siu Yin <siuyin@beyondbroadcast.com>
    Date:   Sat Jun 13 13:20:02 2026 +0800
    
        wip: annotations
    
    commit d8358b447e23da71ec26c06f66e0987112888171
    Author: Loh Siu Yin <siuyin@beyondbroadcast.com>
    Date:   Thu Jun 11 20:26:00 2026 +0800
    
        refactor: use 32F matrix type

---
+++ cmd/07_annotations/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+
+	"gocv.io/x/gocv"
+)
+
+const imgFile = "images/dog.png"
+
+func main() {
+	img := gocv.IMRead(imgFile, gocv.IMReadUnchanged)
+	defer img.Close()
+
+	fmt.Println("size:", img.Size())
+	w := display("Source", img)
+	defer w.Close()
+
+	ln := line(img, image.Point{133, 144}, image.Point{259, 133}, color.RGBA{0, 32, 200, 64}, 32)
+	defer ln.Close()
+	wLn := display("Line", ln)
+	defer wLn.Close()
+
+	c := circ(img, image.Point{196, 174}, 24, color.RGBA{255, 0, 0, 127}, -1) // use thickness of -1 for filled circle
+	defer c.Close()
+	wC := display("Circle", c)
+	defer wC.Close()
+
+	fmt.Println("Press any key to close window.")
+	w.WaitKey(0)
+}
+
+func display(title string, img gocv.Mat) *gocv.Window {
+	w := gocv.NewWindow(title)
+	if err := w.ResizeWindow(img.Cols(), img.Rows()); err != nil {
+		log.Fatal(err)
+	}
+	w.IMShow(img)
+	return w
+}
+
+func line(src gocv.Mat, start, end image.Point, c color.RGBA, thickness int) gocv.Mat {
+	ovr := src.Clone()
+	if err := gocv.RectangleWithParams(&ovr, image.Rectangle{start, end}, c, thickness, gocv.LineAA, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	alpha := float64(c.A) / 255.0
+	img := alphaBlend(src, ovr, alpha)
+	return img
+}
+
+func circ(src gocv.Mat, ctr image.Point, radius int, c color.RGBA, thickness int) gocv.Mat {
+	ovr := src.Clone()
+	defer ovr.Close()
+
+	if err := gocv.CircleWithParams(&ovr, ctr, radius, c, thickness, gocv.LineAA, 0); err != nil {
+		log.Fatal(err)
+	}
+	alpha := float64(c.A) / 255.0
+	img := alphaBlend(src, ovr, alpha)
+
+	return img
+}
+
+func alphaBlend(src, ovr gocv.Mat, alpha float64) gocv.Mat {
+	img := gocv.NewMat()
+	if err := gocv.AddWeighted(src, 1-alpha, ovr, alpha, 0, &img); err != nil {
+		log.Fatal(err)
+	}
+	return img
+}
